981dca1a1434a4de51260e8ed0651e2b3fddc374
FurrG <A2674151a>
2019-11-06 11:20:45 +0200

    varProductSum__countUpdate.

--- varProductSum/js/script.js
+++ varProductSum/js/script.js
@@ -1,11 +1,15 @@
+let productPrice = [0];
+console.log(productPrice.length)
+let total = 0;
 document.getElementById("deleteLine").addEventListener("click", function () {
     $(".adder:first").remove();
+    productPrice.pop();
 });
-document.getElementById("calcButton").addEventListener("click", function () {
+document.getElementById("adder").addEventListener("click", function () {
     let Name = document.getElementById("name").value;
     let Price = document.getElementById("price").value;
     let Amount = document.getElementById("amount").value;
-    let total =+ (Price * Amount);
+    productPrice.push(Price * Amount);
     $(".addPoint").after("<div class=\"row m-0 bg-light adder border border-dark\">\n" +
         "        <div class=\"col-3 pt-4 pb-4\">\n" +
         Name +
@@ -18,5 +22,17 @@ document.getElementById("calcButton").addEventListener("click", function () {
         "</div>\n" +
         "        <div class=\"col-3 pt-4 pb-4\"> </div>\n" +
         "    </div>");
-    $("#totalId").html(total + '')
 });
+setInterval(() => {
+    for (let i = 0; i < productPrice.length; i++) {
+        total = +productPrice[i];
+    }
+    $("#totalId").html(total + '')
+}, 1000);
+setInterval(() => {
+    if (productPrice.length >= 11) {
+        $("#adderBox").css({'display': 'none'})
+    }else {
+        $("#adderBox").css({'display': 'block'}) 
+    }
+}, 1000);
