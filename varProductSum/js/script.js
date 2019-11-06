12769d712176491b1c994cd271cbb62d99bc1f00
FurrG <A2674151a>
2019-11-06 14:05:06 +0200

    varProductSum__countUpdate__logic_ang_idea_fix.

--- varProductSum/js/script.js
+++ varProductSum/js/script.js
@@ -1,38 +1,37 @@
 let productPrice = [0];
-console.log(productPrice.length)
 let total = 0;
 document.getElementById("deleteLine").addEventListener("click", function () {
     $(".adder:first").remove();
     productPrice.pop();
 });
 document.getElementById("adder").addEventListener("click", function () {
-    let Name = document.getElementById("name").value;
-    let Price = document.getElementById("price").value;
-    let Amount = document.getElementById("amount").value;
-    productPrice.push(Price * Amount);
+    let name = document.getElementById("name").value;
+    let price = document.getElementById("price").value;
+    let amount = document.getElementById("amount").value;
+    let sum = price * amount;
+    productPrice.push(sum);
     $(".addPoint").after("<div class=\"row m-0 bg-light adder border border-dark\">\n" +
         "        <div class=\"col-3 pt-4 pb-4\">\n" +
-        Name +
+        name +
         "</div>\n" +
         "        <div class=\"col-3 border-left border-right border-dark pt-4 pb-4\">\n" +
-        Price +
+        price +
         "</div>\n" +
         "        <div class=\"col-3 border-right border-dark pt-4 pb-4\">\n" +
-        Amount +
+        amount +
         "</div>\n" +
-        "        <div class=\"col-3 pt-4 pb-4\"> </div>\n" +
+        "        <div class=\"col-3 pt-4 pb-4\">" + sum +
+        "</div>\n" +
         "    </div>");
 });
 setInterval(() => {
     for (let i = 0; i < productPrice.length; i++) {
         total = +productPrice[i];
     }
-    $("#totalId").html(total + '')
-}, 1000);
-setInterval(() => {
+    $("#totalId").html(total + '');
     if (productPrice.length >= 11) {
         $("#adderBox").css({'display': 'none'})
     }else {
-        $("#adderBox").css({'display': 'block'}) 
-    }
+        $("#adderBox").css({'display': 'block'})
+    };
 }, 1000);
